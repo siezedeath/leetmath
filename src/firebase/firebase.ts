7c74dc284be811b34f7ca0944ae6ec7d62e274fb
alfardil <fardilfayed@gmail.com>
2024-07-27 20:35:42 -0400

    initial commmit

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,30 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDWFLJSTDrLTr_y1dsBjCjbPSUtRrAduYY",
-  authDomain: "leetmath-2470f.firebaseapp.com",
-  projectId: "leetmath-2470f",
-  storageBucket: "leetmath-2470f.appspot.com",
-  messagingSenderId: "700808609923",
-  appId: "1:700808609923:web:a6a22cb4eeecec16671bb6",
-  measurementId: "G-5T9TBFEMGT"
+	apiKey: "AIzaSyCLEhoObEcFlMIEP5jpieJAj3Q1ulFt2iE",
+	authDomain: "leetcodemath.firebaseapp.com",
+	projectId: "leetcodemath",
+	storageBucket: "leetcodemath.appspot.com",
+	messagingSenderId: "57179098773",
+	appId: "1:57179098773:web:1a305dfa8f293310b9559c"
 };
 
-// Initialize Firebase
-const app =!getApps.length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 
-const analytics = getAnalytics(app);
-
 export { auth, firestore, app };
-
