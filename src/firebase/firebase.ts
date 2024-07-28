285fed3ed44c9e2dd31aec0a0ccbff6542cd1c29
Alfayed Alam <fayedfardil2@gmail.com>
2024-07-27 21:57:35 -0400

    updated firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,5 +22,8 @@ const firebaseConfig = {
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
+const firestore = getFirestore
+
 const analytics = getAnalytics(app);
+
 export { auth, firestore, app};
