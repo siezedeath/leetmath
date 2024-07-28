41777f975361055db4b8e2efd2eef48dc29598b6
Alfayed Alam <fayedfardil2@gmail.com>
2024-07-27 23:13:45 -0400

    bug fixed taht was annoying everyone firebase

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -8,18 +8,18 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDWFLJSTDrLTr_y1dsBjCjbPSUtRrAduYY",
-  authDomain: "leetmath-2470f.firebaseapp.com",
-  projectId: "leetmath-2470f",
-  storageBucket: "leetmath-2470f.appspot.com",
-  messagingSenderId: "700808609923",
-  appId: "1:700808609923:web:a6a22cb4eeecec16671bb6",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
-const firestore = getFirestore
+const firestore = getFirestore(app);
 
 export { auth, firestore, app};
